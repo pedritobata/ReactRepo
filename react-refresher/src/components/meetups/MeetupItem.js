91e7c42e618f961c7081304fca003a3f3b7bd957
pedro martinez <pedromartinez_goyo@hotmail.com>
2021-07-19 08:56:36 -0500

    Added Favorites context

--- react-refresher/src/components/meetups/MeetupItem.js
+++ react-refresher/src/components/meetups/MeetupItem.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import FavoritesContext from '../../store/favorites-context';
 import classes from './MeetupItem.module.css';
 
+
 const MeetupItem = (props) => {
+    const {addFavorite, removeFavorite, isFavorite} = useContext(FavoritesContext);
+
+    const toggleFavoritesHandler = () => {
+        if(isFavorite(props.id)){
+            removeFavorite(props.id);
+        } else {
+            addFavorite({
+                id: props.id,
+                title: props.title,
+                image: props.image,
+                address: props.address,
+                description: props.description
+            })
+        }
+    }
 
     return (
         <li className={classes.item}>
@@ -14,7 +31,7 @@ const MeetupItem = (props) => {
                 <p>{props.description}</p>
             </div>
             <div className={classes.actions}> 
-                <button>To Favorites 💖</button>
+                <button onClick={toggleFavoritesHandler}>{isFavorite(props.id) ? 'Remove from favorites' : 'To Favorites 💖'}</button>
             </div>
         </li>
     )
