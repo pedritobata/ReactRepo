bff4cc64bb1ded2263986bb87cd04caa3ad00fba
pedro martinez <pedromartinez_goyo@hotmail.com>
2022-06-26 18:00:04 -0500

    booking-app: auth, login feature

--- booking-app/backend/index.js
+++ booking-app/backend/index.js
@@ -1,10 +1,16 @@
+import cookieparser from 'cookie-parser';
 import app from './server.js';
 import userRoutes from './src/routes/user.routes.js';
 import hotelRoutes from './src/routes/hotel.routes.js';
+import authRoutes from "./src/routes/auth.routes.js";
 
+// Middlewares
+app.use(cookieparser());
 
+// routes
 app.use('/api/users', userRoutes);
 app.use('/api/hotels', hotelRoutes);
+app.use('/api/auth', authRoutes);
 
 app.use((err, req, res, next) => {
     const status = err.status || 500;
